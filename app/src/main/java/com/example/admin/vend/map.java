f48252c1cfc1d4315492a51affc3e442018a5093
Nolan Blankenau <nolanblankenau@gmail.com>
2018-10-19 21:39:55 -0500

    Changed classes to activities

--- app/src/main/java/com/example/admin/vend/map.java
+++ app/src/main/java/com/example/admin/vend/map.java
@@ -1,4 +1,14 @@
 package com.example.admin.vend;
 
-public class map {
+import android.os.Bundle;
+import android.app.Activity;
+
+public class map extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map2);
+    }
+
 }
