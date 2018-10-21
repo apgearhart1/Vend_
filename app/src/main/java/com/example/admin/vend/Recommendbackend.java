44debe9c7ffe51645c5f34f86f7a19732e657067
ColinSherman <bowsherman@gmail.com>
2018-10-21 04:55:05 -0500

    Clear notification after user clicks Going or Not Going

--- app/src/main/java/com/example/admin/vend/Recommendbackend.java
+++ app/src/main/java/com/example/admin/vend/Recommendbackend.java
@@ -1,5 +1,6 @@
 package com.example.admin.vend;
 
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -10,19 +11,11 @@ public class Recommendbackend extends BroadcastReceiver {
     private static final String TAG = "MyBroadcastReceiver";
     @Override
     public void onReceive(Context context, Intent intent) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Action: " + intent.getAction() + "\n");
-        System.out.println("HERERERE " + intent.getBooleanExtra("going", false));
-        sb.append("URI: " + intent.toUri(Intent.URI_INTENT_SCHEME).toString() + "\n");
-        String log = sb.toString();
-        Log.d(TAG, log);
-        Toast.makeText(context, log, Toast.LENGTH_LONG).show();
-
         if(intent.getBooleanExtra("going", false)) {
             Sekretz.train(Notifications.lat, Notifications.lng, Notifications.category, 1);
         } else {
             Sekretz.train(Notifications.lat, Notifications.lng, Notifications.category, 0);
         }
-
+        ((NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE)).cancel(Notifications.NOTIFICATION_ID);
     }
 }
--- app/src/main/java/com/example/admin/vend/signup.java
+++ app/src/main/java/com/example/admin/vend/signup.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
