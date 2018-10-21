dfaa330de12326d87aa19f9d9c05bde31037c2a6
ColinSherman <bowsherman@gmail.com>
2018-10-21 05:51:52 -0500

    Added dropdown for category

--- app/src/main/java/com/example/admin/vend/addSpot.java
+++ app/src/main/java/com/example/admin/vend/addSpot.java
@@ -4,10 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class addSpot extends AppCompatActivity {
+
+    private Map<String, String> categories = new HashMap<String, String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +28,20 @@ public class addSpot extends AppCompatActivity {
         final EditText latitude = (EditText)findViewById(R.id.latitude);
         final EditText open = (EditText)findViewById(R.id.open);
         final EditText close = (EditText)findViewById(R.id.close);
-        final EditText category = (EditText)findViewById(R.id.category);
+        final Spinner category = (Spinner)findViewById(R.id.category);
 
         Button currentLocation = (Button)findViewById(R.id.currentLocation);
         Button add = (Button)findViewById(R.id.add);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.categories, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        category.setAdapter(adapter);
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 database data = new database();
-                data.createVendor(name.getText().toString(), Double.parseDouble(longitude.getText().toString()), Double.parseDouble(latitude.getText().toString()), open.getText().toString(), close.getText().toString(), category.getText().toString());
+                data.createVendor(name.getText().toString(), Double.parseDouble(longitude.getText().toString()), Double.parseDouble(latitude.getText().toString()), open.getText().toString(), close.getText().toString(), categories.get(((TextView) category.getSelectedView()).getText().toString()));
 
             }
         });
@@ -42,6 +55,14 @@ public class addSpot extends AppCompatActivity {
 
             }
         });
-    }
 
+        categories.put("Chinese food", "1");
+        categories.put("Mexican food", "2");
+        categories.put("Breakfast food", "3");
+        categories.put("Ice cream", "4");
+        categories.put("Jewelry", "5");
+        categories.put("Art", "6");
+        categories.put("Apparel", "7");
+        categories.put("Hot dogs", "8");
+    }
 }
